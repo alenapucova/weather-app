a91239cea26d4bd09bf40c39f5a02ed3a6e00784
Alena Pucova <alena.pucova@centrum.cz>
2019-07-09 13:03:58 +0200

    Detailed forecast

--- src/App.js
+++ src/App.js
@@ -16,8 +16,9 @@ class App extends React.Component {
     min_temp: [],
     condition: [],
     humidity: [],
+    details: [],
     error: undefined,
-    loading:undefined
+    loading:undefined,
   }
   getWeather = async (e) => {
 
@@ -28,26 +29,37 @@ class App extends React.Component {
     this.setState({
       loading: true
     })
-
+    
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${city},${country}&appid=${API_KEY}&units=metric`);
     const data = await api_call.json();
 
     this.setState({
       loading: false
     })
+    //separating array into 5 single days
+    var todaySlices = this.getNumberOfTodaySlices(data);
+    var slicesPerDay = 8;
 
+    var day1 = data.list.slice(0,todaySlices);
+    var day2 = data.list.slice(todaySlices,todaySlices + slicesPerDay);
+    var day3 = data.list.slice(todaySlices + slicesPerDay, todaySlices + slicesPerDay * 2);
+    var day4 = data.list.slice(todaySlices + slicesPerDay * 2, todaySlices + slicesPerDay * 3);
+    var day5 = data.list.slice(todaySlices + slicesPerDay * 3, todaySlices + slicesPerDay * 4);
+    
     if (city && country) {
       console.log(data);
       this.setState({
         city: data.city.name,
         country: data.city.country,
         day: ['Today', 'Tomorrow', this.getDay(data.list[15].dt_txt), this.getDay(data.list[23].dt_txt), this.getDay(data.list[31].dt_txt)],
-        max_temp: [data.list[0].main.temp_max, data.list[7].main.temp_max, data.list[15].main.temp_max, data.list[23].main.temp_max, data.list[31].main.temp_max],
-        min_temp: [data.list[0].main.temp_min, data.list[7].main.temp_min, data.list[15].main.temp_min, data.list[23].main.temp_min, data.list[31].main.temp_min],
+        max_temp: [this.getMaxTemperature(day1),this.getMaxTemperature(day2),this.getMaxTemperature(day3),this.getMaxTemperature(day4),this.getMaxTemperature(day5)],
+        min_temp: [this.getMinTemperature(day1),this.getMinTemperature(day2),this.getMinTemperature(day3),this.getMinTemperature(day4),this.getMinTemperature(day5)],
         condition: [data.list[0].weather[0].description, data.list[7].weather[0].description, data.list[15].weather[0].description, data.list[23].weather[0].description, data.list[31].weather[0].description],
         humidity: [data.list[0].main.humidity, data.list[7].main.humidity, data.list[15].main.humidity, data.list[23].main.humidity, data.list[31].main.humidity],
-        error: ""
-      })
+        details:  [this.getDetails(day1),this.getDetails(day2),this.getDetails(day3),this.getDetails(day4),this.getDetails(day5)],
+        error: "",
+      })      
+      console.log(this.state.max_temp)
     } else {
       this.setState({
         city: undefined,
@@ -56,12 +68,67 @@ class App extends React.Component {
       })
     }
   }
+  getNumberOfTodaySlices = (data) => {
+    
+    var todayForecast = 0;
+    var endOfDay = 23;
+    var date = new Date(data.list[0].dt * 1000);
+    var today = date.getHours();
+    while (today < endOfDay){
+      todayForecast++;
+      today++;
+    }
+    
+    console.log(todayForecast/3);
+    return (todayForecast/3) +1 ;
+  }
+  
+  getMinTemperature = (singleDay) => {
+    var min = [];
+
+    for (var i = 0;i < singleDay.length;i++){
+      min.push(singleDay[i].main.temp_min)
+    }
+    console.log(min)
+    return(Math.round(Math.min(...min)))
+  }
+  getMaxTemperature = (singleDay) => {
+    var max = [];
+
+    for (var i = 0; i < singleDay.length; i++){
+      max.push(singleDay[i].main.temp_max)
+    }
+    console.log(max)
+    return(Math.round(Math.max(...max)))
+  }
+  //detailed temperature for each day
+  getDetails = (singleDay) => {
+    var weatherDetail = [];
+        
+    for (var i= 0; i < singleDay.length; i++){
+      weatherDetail.push(this.getTime(singleDay[i].dt_txt)+ "---",Math.round(singleDay[i].main.temp_max) + " °C ")
+    }
+    
+    return weatherDetail;
+  }
   getDay = (myDate) => {
     const date = new Date(myDate);
     var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     return days[date.getDay()];
   }
+  getTime = (myDate) => {
+    const date = new Date(myDate);
+    const hour = date.getHours().toString();
+    var time = "";
 
+    if(hour < 12){
+      time = " am"
+    }else{
+      time = " pm"
+    }
+    return hour + time ;
+  }
+ 
   render() {
     return (
       
@@ -77,7 +144,8 @@ class App extends React.Component {
           error={this.state.error}
         />
         <div className="forecast">
-          <Weather
+          <Weather 
+            id={1}
             city={this.state.city}
             country={this.state.country}
             day={this.state.day[0]}
@@ -85,8 +153,10 @@ class App extends React.Component {
             min_temp={this.state.min_temp[0]}
             condition={this.state.condition[0]}
             humidity={this.state.humidity[0]}
+            details={this.state.details[0]}
           />
-          <Weather
+          <Weather 
+            id={2}
             city={this.state.city}
             country={this.state.country}
             day={this.state.day[1]}
@@ -94,8 +164,10 @@ class App extends React.Component {
             min_temp={this.state.min_temp[1]}
             condition={this.state.condition[1]}
             humidity={this.state.humidity[1]}
+            details={this.state.details[1]}
           />
-          <Weather
+          <Weather 
+            id={3}
             city={this.state.city}
             country={this.state.country}
             day={this.state.day[2]}
@@ -103,8 +175,10 @@ class App extends React.Component {
             min_temp={this.state.min_temp[2]}
             condition={this.state.condition[2]}
             humidity={this.state.humidity[2]}
+            details={this.state.details[2]}
           />
-          <Weather
+          <Weather 
+            id={4}
             city={this.state.city}
             country={this.state.country}
             day={this.state.day[3]}
@@ -112,8 +186,10 @@ class App extends React.Component {
             min_temp={this.state.min_temp[3]}
             condition={this.state.condition[3]}
             humidity={this.state.humidity[3]}
+            details={this.state.details[3]}
           />
-          <Weather
+          <Weather 
+            id={5}
             city={this.state.city}
             country={this.state.country}
             day={this.state.day[4]}
@@ -121,6 +197,7 @@ class App extends React.Component {
             min_temp={this.state.min_temp[4]}
             condition={this.state.condition[4]}
             humidity={this.state.humidity[4]}
+            details={this.state.details[4]}
           />
         </div>
       </div>
--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -1,7 +1,5 @@
 import React from "react"
 
-
-
 const pStyle = {
     fontWeight: 'bold',
 }
@@ -11,13 +9,20 @@ class Weather extends React.Component {
     render() {
         return (
 
-            <div className="single_day" >
+            <div className="singleDay" >
                 {this.props.day && <p style={pStyle}> {this.props.day}</p>}
                 {this.props.max_temp && <p>Max temperature:  {this.props.max_temp}°C</p>}
                 {this.props.min_temp && <p>Min temperature: {this.props.min_temp}°C</p>}
                 {this.props.condition && <p>Condition: {this.props.condition}</p>}
                 {this.props.humidity && <p>Humidity: {this.props.humidity}</p>}
                 {this.props.error && <p>{alert("Please enter the location")}</p>}
+                {this.props.city && <button className="btn btn-primary collapsed" data-toggle="collapse" data-target={"#demo" + this.props.id}>
+                    <span className="hideDetails">Hide details</span>
+                    <span className="seeDetails">See details</span>
+                </button>}
+                <div id={"demo" + this.props.id} className="collapse">
+                    {this.props.details} 
+                </div>
             </div>
         )
     }
